f9fd9118135995dd10b8808f2908f15778590d80
Varija Agarwal <agarwalvarija@gmail.com>
2017-12-02 16:29:45 -0500

    Initial commit

---
+++ designs.js
@@ -0,0 +1,34 @@
+$(function() {
+  //Submit button clicked --> call makeGrid() function
+  const $gridSubmit = $('#grid-size-submit'); // cache jQuery object for submit button
+  let $gridTable = $('#pixel_canvas');
+
+  $gridSubmit.click(function makeGrid(e) {
+    //get the values from grid height and grid width inputs
+    e.preventDefault();
+    const yBlocks = $('#input_height').val();
+    const xBlocks = $('#input_width').val();
+
+    // create table data elements consisting of xBlocks horizontally and yBlocks vertically
+    // note that grid height and width don't have to necessarily be equal
+    let gridHTML = '';
+    for (let row=1; row<=xBlocks; row++) {
+      gridHTML += '<tr>';
+      for (let col=1; col<=yBlocks; col++) {
+        gridHTML += '<td></td>';
+      }
+      gridHTML += '</tr>';
+    }
+    // add those table data elements to the <table> in index.html
+    $gridTable.html(gridHTML);
+  });
+
+  // when user clicks on a block, change the color of that block
+  // I personally like this app better when the event is mouseover
+  $gridTable.on('click', 'td', function(e) {
+    e.preventDefault();
+    // get the value of the color input
+    const chosenColor = $('#colorPicker').val();
+    $(this).css('background-color', chosenColor);
+  });
+});
